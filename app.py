009e988b55f70e942b07847cb77cfbc9172347b1
Mody-B <mohamad.bitar05@gmail.com>
2026-05-03 14:31:19 +0300

    fix upload error in deployment

--- app.py
+++ app.py
@@ -151,3 +151,8 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
 
 os.makedirs('uploads', exist_ok=True)    
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
